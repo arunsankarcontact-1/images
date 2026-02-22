76520e06c0ba5d2ad23fba0ffd9d1505bb13a5ca
arun <arunsankar.contact@gmail.com>
2026-02-22 13:37:39 +0530

    Updated the userdata with original dockerhub username

--- ec2_userdata.sh
+++ ec2_userdata.sh
@@ -4,5 +4,5 @@ yum install -y docker
 systemctl start docker
 systemctl enable docker
 
-docker pull DOCKERHUB_USERNAME/dev-nginx:latest
-docker run -d -p 80:80 DOCKERHUB_USERNAME/dev-nginx:latest
+docker pull aruns14/dev-nginx:latest
+docker run -d -p 80:80 aruns14/dev-nginx:latest
